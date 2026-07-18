fd24c9a873057ea4ff043dfb8aa7f53289e48a0c
Hodali-1 <Gatetehodali2@gmail.com>
2026-07-18 15:38:18 -0700

    python-classes: replace 0-my_first_class with correct 0-square

--- python-classes/0-my_first_class.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python3
-"""Defines an empty class used as the starting point for this project."""
-
-
-class MyClass:
-    """Represent an empty class with no attributes or methods yet."""
