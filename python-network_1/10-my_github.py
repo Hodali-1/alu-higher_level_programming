86f5dce1ac12a7c075be89a315022bbcb764728d
Hodali-1 <Gatetehodali2@gmail.com>
2026-08-08 07:59:45 +0000

    python-network_1: fix pycodestyle in 10-my_github.py

--- python-network_1/10-my_github.py
+++ python-network_1/10-my_github.py
@@ -5,6 +5,6 @@ import requests
 
 
 if __name__ == "__main__":
-    r = requests.get("https://api.github.com/user",
-                      auth=(sys.argv[1], sys.argv[2]))
+    url = "https://api.github.com/user"
+    r = requests.get(url, auth=(sys.argv[1], sys.argv[2]))
     print(r.json().get("id"))
